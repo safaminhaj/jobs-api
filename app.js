22c82a7b05768878e23785bd8ba0652dae7830a5
safaminhaj <safaminhaj11@gmail.com>
2024-07-17 20:13:40 +0530

    authentication middleware implemented

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 //connectDB
 const connectDB = require("./db/connect");
 
+const authenticateUser = require("./middleware/authentication");
+
 //routers
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
@@ -18,7 +20,7 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
